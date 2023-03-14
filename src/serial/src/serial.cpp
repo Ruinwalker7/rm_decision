35c9c66b2a57d37fb3dd8da5a74c8a9ad877ac93
chen <973521624@qq.com>
2023-03-14 19:56:17 +0800

    动态serial

--- src/serial/src/serial.cpp
+++ src/serial/src/serial.cpp
@@ -1,18 +1,26 @@
 #include <ros/ros.h>
-// #include <rm_decision/SerialReceiveMsg.h>
+#include "rm_interfaces/SerialReceiveMsg.h"
+#include "configure/SerialConfig.h"
+
+static int mode = 0;
+static int brood=600;
+void configure_cb(const serial::SerialConfig &config, uint32_t level) {
+  mode = config.mode;
+  brood =config.brood;
+}
 
 int main(int argc, char** argv) {
   // create ros2 node
   ros::init(argc, argv, "serial");
   ros::NodeHandle _nh;
-    // ros::Publisher serial_pub = _nh.advertise<rm_decision::SerialReceiveMsg>("/serial_receive",2);
-    // rm_decision::SerialReceiveMsg msg;
-    // msg.pitch=0;
-    // msg.yaw=0;
-    // msg.time = 420;
-    // msg.blood=600;
-    // msg.mode=0;
-    // msg.header.stamp=ros::Time::now();
+    ros::Publisher serial_pub = _nh.advertise<rm_interfaces::SerialReceiveMsg>("/serial_receive",2);
+    rm_interfaces::SerialReceiveMsg msg;
+    msg.pitch=0;
+    msg.yaw=0;
+    msg.time = 420;
+    msg.blood=brood;
+    msg.mode=mode;
+    msg.header.stamp=ros::Time::now();
 
     while (ros::ok()) {
         // serial_pub.publish(msg);
